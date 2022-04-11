48025dc4e80f5b6aa46b144c26bdc39aff0a9325
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-04-11 11:51:45 +0200

    Ändrade tillbaka till den ursprungliga
    Är mer effektiv.

--- PreProcessing_test3.m
+++ PreProcessing_test3.m
@@ -1,36 +1,29 @@
-
-
-% ---------------- Preprocessing -----------------------------
-% ---------------- 1. fitering 
-%------------------2. image generation 
-%------------------3. resizing 
-
-close all
-clearvars
-
 % sample data information
-SampleSec = 0.5; %every 5 second
-StartSampleSec = 0.4;  %start time
+SampleSec = 1; %every 5 second
+StartSampleSec = 1;  %start time
 StepSize = 1000; 
 %Path = 'C:\IoTaP-CoSIS-DISS\Projects\Database\ES2002\';  %dataset path
 Path = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\recorded_audio\'
-PathTo = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\mixed\'
+PathTo = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\ourrecordings\'
+
 %FileName = 'EN2001b.Headset-0'; 
-FileName = ['recording1']
-FileName2write = 'recording1';
+FileName = ['mySpeech']
+FileName2write = '2022-04-08';
 [signal,fs] = audioread(strcat(Path,FileName,'.wav'));
 %sound(singal,fs)
 
 whos signal
 whos fs
 
+
 %filtering
 %%
 
 
 % l = length(signal);
-% signal = signal(signal > 0.0005 | signal < - 0.0005);
+%  signal1 = signal(signal > 0.0005 | signal < - 0.0005);
 % audiowrite(strcat(Path,FileName2write,'.wav'),signal,fs)
+
 len = length(signal);
 TotalTime = len./fs;
 dt=1/fs;
@@ -38,54 +31,58 @@ dt=1/fs;
 time = 0:dt:TotalTime-dt;
 
 %plot(psd(spectrum.periodogram,signal,'Fs',fs,'NFFT',length(signal)));
-
-% plot(time, signal); xlabel('Seconds'); ylabel('Amplitude');
-
+% 
+%  plot(time, signal); xlabel('Seconds'); ylabel('Amplitude');
+%%
 SampleNums = SampleSec*fs;
 StartSampleNum = StartSampleSec*fs;
 
-maxi = floor(len/StartSampleNum)/10;
+maxi = floor(len/StartSampleNum);
 %Magnitude Scalogram
+
 
 maxi
 imgIndex = 1;
 
 % generate Continuous Wavelet Transform (CWT)
-for i=1:maxi-1
-    StartPoint =(i-1)*StartSampleNum+1;
+for i=1:maxi - 1
+  StartPoint =(i-1)*StartSampleNum+1;
+        StartPoint =(i-1)*StartSampleNum+1;
     mtlb = signal(StartPoint:StartPoint+SampleNums);
-    [cfs,frq] = cwt(mtlb, fs);
+    [cfs,frq] = cwt(mtlb,fs,'ExtendSignal',true);
+    tms = (0:numel(mtlb)-1)/fs;
+    surface(tms(:,1:StepSize:end),frq,abs(cfs(:,1:StepSize:end)));
+        saveas(gcf,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'))
 
-     tms = (0:numel(mtlb)-1)/fs;
-     surface(tms,frq,abs(cfs))
-     axis tight
-    shading flat
-%     xlabel('Time (s)')
-%     ylabel('Frequency (Hz)')
-    set(gca,'yscale','log')
     set(gca, 'Visible', 'off')
-    colorbar('off');    
+    colorbar('off');
+
     InSet = get(gca, 'TightInset');
-     set(gca, 'Position', [InSet(1:2), 1-InSet(1)-InSet(3), 1-InSet(2)-InSet(4)]);
+    set(gca, 'Position', [InSet(1:2), 1-InSet(1)-InSet(3), 1-InSet(2)-InSet(4)]);
 
+    axis tight
+    shading flat
+    %xlabel('Time (s)')
+    %ylabel('Frequency (Hz)')
+    set(gca,'yscale','log')
 
-   
-  saveas(gcf,strcat("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\ourrecordings\2022-04-08",'\cwt_',int2str(imgIndex),'.jpg'))
+    saveas(gcf,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'))
 
-
-    I = imread(strcat("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\ourrecordings\2022-04-08",'\cwt_',int2str(imgIndex),'.jpg'));
+    %Resize the image to make the process faster
+    I = imread(strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'));
     J = imresize(I, 0.5);
-    imwrite(J,strcat(strcat("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\ourrecordings\2022-04-08",'\cwt_',int2str(imgIndex),'.jpg')));
-    imgIndex = imgIndex+1;
+    imwrite(J,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'));
 
     i
     maxi
-    
-%     clear InSet;
+%     length(cfs)
+%     length(frq)
+
+    clear InSet;
     clear mtlb;
     clear cfs;
-%     clear I;
-%     clear J;
+    clear I;
+    clear J;
 end
 
 %%
@@ -106,6 +103,3 @@ end
 % xlabel('Time (s)')
 % ylabel('Frequency (Hz)')
 % set(gca,'yscale','log')
-
-
-
