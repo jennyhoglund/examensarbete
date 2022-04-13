5d5ab51985fc8e2db9446b83ad0146ca756e1928
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-04-13 09:21:33 +0200

    Update preprocessingWithVAD.m

--- preprocessingWithVAD.m
+++ preprocessingWithVAD.m
@@ -8,8 +8,8 @@
 close all
 clearvars
 
-[audioIn] = audioread("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\recorded_audio\2022-04-08.wav");
-fileReader = dsp.AudioFileReader("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\recorded_audio\2022-04-08.wav");
+[audioIn] = audioread("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\TS3011a\audio\TS3011a-2_filtered.wav");
+fileReader = dsp.AudioFileReader("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\TS3011a\audio\TS3011a-2_filtered.wav");
 fs = fileReader.SampleRate
 fileReader.SamplesPerFrame = ceil(10e-3*fs);
 VAD = voiceActivityDetector;
@@ -23,30 +23,36 @@ VAD = voiceActivityDetector;
 %     'ShowLegend',true, ...
 %     'ChannelNames',{'Audio','Probability of speech presence'});
 %deviceWriter = audioDeviceWriter('SampleRate',fs);
-fileWriter = dsp.AudioFileWriter('mySpeech.wav','FileFormat','WAV', 'SampleRate', 16000);
-while ~isDone(fileReader)
+fileWriter = dsp.AudioFileWriter("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\TS3011a\audio\TS3011a-2_VAD.wav",'FileFormat','WAV', 'SampleRate', 16000)
+
+
+%%
+i = 1;
+while ~isDone(fileReader) || i <100
     audioIn = fileReader();
     probability = VAD(audioIn);
     if probability >0.95
-        %disp('it is over 95%')
+        
         fileWriter(audioIn);
     end
  %   scope(audioIn,probability*ones(fileReader.SamplesPerFrame,1))
   %  deviceWriter(audioIn);
+  i = i+1;
 end
 release(fileReader)
 release(fileWriter)
+whos audioIn
 
 %%
 
 deviceReader = audioDeviceReader();
 setup(deviceReader)
-fileWriter = dsp.AudioFileWriter('mySpeech.wav','FileFormat','WAV');
+fileWriter = dsp.AudioFileWriter('2022-04-12.wav','FileFormat','WAV');
 VAD = voiceActivityDetector;
 disp('Speak into microphone now.')
 tic
 
-while toc < 20
+while toc < 10
     acquiredAudio = deviceReader();
     probability = VAD(acquiredAudio);
     if probability >0.95
@@ -57,9 +63,6 @@ end
 disp('Recording complete.')
 release(deviceReader)
 release(fileWriter)
+whos acquiredAudio
 
-
-
-
-%%
 
