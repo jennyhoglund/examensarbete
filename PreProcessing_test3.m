27fafe7773d08fe151f61ca45c6c17ff136331ce
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-04-14 16:45:31 +0200

    lade till close all

--- PreProcessing_test3.m
+++ PreProcessing_test3.m
@@ -1,16 +1,24 @@
+
+
+% ---------------- Preprocessing -----------------------------
+% ---------------- 1. fitering 
+%------------------2. image generation 
+%------------------3. resizing 
+
+close all
+clearvars
+
 % sample data information
-SampleSec = 1; %every 5 second
-StartSampleSec = 1;  %start time
+SampleSec = 5; %every 5 second
+StartSampleSec = 5;  %start time
 StepSize = 1000; 
 %Path = 'C:\IoTaP-CoSIS-DISS\Projects\Database\ES2002\';  %dataset path
-Path = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\recorded_audio\'
-PathTo = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\ourrecordings\'
-
-%FileName = 'EN2001b.Headset-0'; 
-FileName = ['mySpeech']
-FileName2write = '2022-04-08';
+Path = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\filtered\testaudio\'
+PathTo = 'C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\filtered\testimages\'
+FileName = 'IS1009a-N'
+PathTo = strcat(PathTo, FileName);
 [signal,fs] = audioread(strcat(Path,FileName,'.wav'));
-%sound(singal,fs)
+
 
 whos signal
 whos fs
@@ -19,10 +27,10 @@ whos fs
 %filtering
 %%
 
-
+% 
 % l = length(signal);
-%  signal1 = signal(signal > 0.0005 | signal < - 0.0005);
-% audiowrite(strcat(Path,FileName2write,'.wav'),signal,fs)
+%  signal = signal(signal > 0.0005 | signal < - 0.0005);
+%  audiowrite(strcat(Path,FileName2write,'.wav'),signal,fs)
 
 len = length(signal);
 TotalTime = len./fs;
@@ -47,14 +55,17 @@ imgIndex = 1;
 % generate Continuous Wavelet Transform (CWT)
 for i=1:maxi - 1
   StartPoint =(i-1)*StartSampleNum+1;
-        StartPoint =(i-1)*StartSampleNum+1;
-    mtlb = signal(StartPoint:StartPoint+SampleNums);
-    [cfs,frq] = cwt(mtlb,fs,'ExtendSignal',true);
+
+    mtlb = signal(StartPoint:StartPoint+SampleNums - 1);
+    whos mtlb
+    fs
+    [cfs,frq] = cwt(mtlb,fs);
     tms = (0:numel(mtlb)-1)/fs;
     surface(tms(:,1:StepSize:end),frq,abs(cfs(:,1:StepSize:end)));
-        saveas(gcf,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'))
+    
 
     set(gca, 'Visible', 'off')
+    set(gca,'yscale','log')
     colorbar('off');
 
     InSet = get(gca, 'TightInset');
@@ -64,14 +75,14 @@ for i=1:maxi - 1
     shading flat
     %xlabel('Time (s)')
     %ylabel('Frequency (Hz)')
-    set(gca,'yscale','log')
+    
 
-    saveas(gcf,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'))
+    saveas(gcf,strcat(PathTo,'\cwt_',int2str(i),'.jpg'))
 
     %Resize the image to make the process faster
-    I = imread(strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'));
+    I = imread(strcat(PathTo,'\cwt_',int2str(i),'.jpg'));
     J = imresize(I, 0.5);
-    imwrite(J,strcat(PathTo,FileName2write,'\cwt_',int2str(i),'.jpg'));
+    imwrite(J,strcat(PathTo,'\cwt_',int2str(i),'.jpg'));
 
     i
     maxi
@@ -83,6 +94,7 @@ for i=1:maxi - 1
     clear cfs;
     clear I;
     clear J;
+    close all
 end
 
 %%
@@ -103,3 +115,6 @@ end
 % xlabel('Time (s)')
 % ylabel('Frequency (Hz)')
 % set(gca,'yscale','log')
+
+
+
