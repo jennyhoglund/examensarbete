a89f765e4387d070b5a76650b35141f7dea01371
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-04-14 20:19:39 +0200

    Lade till close all

--- realtimeAudiofile.m
+++ realtimeAudiofile.m
@@ -3,41 +3,34 @@ clc
 close all
 load classifier.mat
 
-
-audioIn = audioread("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\IS1009a-3filter.wav");
-VAD = voiceActivityDetector;
-fileReader = dsp.AudioFileReader("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\IS1009a-3filter.wav");
-fileWriter = dsp.AudioFileWriter('recorded_audio\IS1009a-3filter_realtime.wav','FileFormat','WAV', 'SampleRate', 16000)
+audioIn = audioread("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\filtered\testaudio\mix.wav");
+fileReader = dsp.AudioFileReader("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\filtered\testaudio\mix.wav");
+%fileWriter = dsp.AudioFileWriter('recorded_audio\IS1009a-3filter_realtime.wav','FileFormat','WAV', 'SampleRate', 16000)
 fs = fileReader.SampleRate
-fileReader.SamplesPerFrame = ceil(10e-3*fs);
+%fileReader.SamplesPerFrame = ceil(10e-3*fs);
+samplesPerFrame = fs/10
+fileReader.SamplesPerFrame = samplesPerFrame ;
 i = 1;
 imindex = 1;
-oneSecond = zeros(160,1);
-StepSize = 500;
-PathTo = "C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\1sec\ISO1009a-3\"
-correctLabels = {''};
-classes = classes(1:55)
-for i = 1: numel(classes)
-    correctLabels(:,i) = {'speaker1'};
-end
-correctLabels = correctLabels';
-correctLabels = categorical(correctLabels)
-predictions = correctLabels;
+oneSecond = zeros(samplesPerFrame,1);
+StepSize = 1000;
+PathTo = "C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\filtered\testimages\"
+predictions ={''};
 
 %%
 
-while ~isDone(fileReader) 
+while ~isDone(fileReader)
+    
     audioIn = fileReader();
-    oneSecond(i:i+159,:) = audioIn; %Ändra så att den sparar hela audioIn, ökar index, osv. upp till en sekund, sen sound. en sekund är 16000Hz
-    i = i +160;
-    if (i >= 16000)
-
+    oneSecond(i:i+samplesPerFrame - 1,:) = audioIn; %Ändra så att den sparar hela audioIn, ökar index, osv. upp till en sekund, sen sound. en sekund är 16000Hz
+    i = i +samplesPerFrame - 1;
+    if (i >= 5*fs)tic
+% 
 %         sound(oneSecond, fs)
-%         pause(1)
-        [cfs,frq] = cwt(oneSecond,fs,'ExtendSignal',true);
+%         pause(5)
+        [cfs,frq] = cwt(oneSecond,fs);
         tms = (0:numel(oneSecond)-1)/fs;
         surface(tms(:,1:StepSize:end),frq,abs(cfs(:,1:StepSize:end)));
-        saveas(gcf,strcat(PathTo,'\cwt_',int2str(i),'.jpg'))
 
         set(gca, 'Visible', 'off')
         colorbar('off');
@@ -54,45 +47,32 @@ while ~isDone(fileReader)
         I = imread(strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
         J = imresize(I, 0.5);
         imwrite(J,strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
+        close all
 
-         prediction = predict(classifier, J)
-
-%         if prediction == 1
-%             predictions(imindex,:) = {'speaker1'};
-%         else 
-%             predictions(imindex,:) = {'speaker2'};
+        [labelIdx, score] = predict(classifier,J)
+%         if max(score) < -0.1
+%             predictions(imindex,:) = cellstr("newspeakr")
+% 
+%         else
+%             predictions(imindex,:) = classifier.Labels(labelIdx)
 %         end
 
+        imindex = imindex +1 ;
         
-        oneSecond = zeros(160,1);
+        oneSecond = zeros(samplesPerFrame,1);
         i = 1;
-        imindex = imindex +1
+        
 
-
+toc
     end
 
-
-    %     probability = VAD(audioIn);
-    %     if probability >0.95
-    %         %disp('it is over 95%')
-    %         fileWriter(audioIn);
-    %     end
-
-
 end
-sound(oneSecond, fs)
+
 release(fileReader)
-release(fileWriter)
+%release(fileWriter)
 
 
 %%
-preds = classes(1:49);
-index = 1;
-
-imdsTest = imageDatastore("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\IS1009features\IS1009a-3filter");
-while imdsTest.hasdata
-
-    prediction = predict(classifier, J)
-
-
-end
+% predictions;
+% eval = sum(predictions == classes)/numel(classes)
+% wrong = predictions(predictions ~= classes)
