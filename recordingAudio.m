b40df50900278215ea11c96c4d08ad6a77475c61
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-05-08 18:05:07 +0200

    Update recordingAudio.m

--- recordingAudio.m
+++ recordingAudio.m
@@ -1,69 +1,63 @@
-% ---------------- Preprocessing -----------------------------
-% ---------------- 1. fitering 
-%------------------2. image generation 
-%------------------3. resizing 
-
 close all
 clearvars
 fs=16000;
 
-PathTo= 'C:\Users\hebam\Desktop\Examensarbete\AMI-corpus\3s signal\Test';
-VAD = voiceActivityDetector;
-deviceReader = audioDeviceReader(44100,256);
+PathTo= 'C:\Users\jenny\OneDrive\Pictures\temp';
+samplesPerFrame=256;
+deviceReader = audioDeviceReader(44100,samplesPerFrame);
 setup(deviceReader)
-fileWriter = dsp.AudioFileWriter('22.wav','FileFormat','WAV');
+fileWriter = dsp.AudioFileWriter('2022-05-06.wav','FileFormat','WAV');
 VAD = voiceActivityDetector;
 imindex = 1;
-samplesPerFrame=256;
-oneSecond = zeros(samplesPerFrame,1);
-StepSize = 100;
+
+oneSecond = zeros(44100,1);
+StepSize = 1000;
 disp('Speak into microphone now.')
 tic
 i=1;
-totoverrun=0;
-while toc < 10
+totaloverrun=0;
+while toc < 60
     [acquiredAudio, numoverrun] = deviceReader();
-    
+    totaloverrun = totaloverrun + numoverrun;
+
     probability = VAD(acquiredAudio);
-    if probability >0.95
-    fileWriter(acquiredAudio);
-    oneSecond(i:i+samplesPerFrame - 1,:) = acquiredAudio;
-    totoverrun=totoverrun+numoverrun;
-    end
-    i = i +samplesPerFrame - 1;
-    
-    if (i >= 44100)
-       tic
-     [cfs,frq] = cwt(oneSecond,fs);
-        tms = (0:numel(oneSecond)-1)/fs;
-        surface(tms(:,1:StepSize:end),frq,abs(cfs(:,1:StepSize:end)));
-
-        set(gca, 'Visible', 'off')
-        colorbar('off');
-        InSet = get(gca, 'TightInset');
-        set(gca, 'Position', [InSet(1:2), 1-InSet(1)-InSet(3), 1-InSet(2)-InSet(4)]);
-        axis tight
-        shading flat
-        set(gca,'yscale','log')
-       saveas(gcf,strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'))
-
-        %Resize the image to make the process faster
-       I = imread(strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
-        J = imresize(I, 0.5);
-       imwrite(J,strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
-        close all
-        imindex= imindex+1;
-
-        oneSecond = zeros(samplesPerFrame,1);
+%     if probability >0.5
+        fileWriter(acquiredAudio);
+        %
+        oneSecond(i:i+samplesPerFrame - 1,:) = acquiredAudio;
+        i = i +samplesPerFrame;
+%     end
+    %
+    %
+    if (i >= 5*44100)
+        [cfs,frq] = cwt(oneSecond,fs);
+                tms = (0:numel(oneSecond)-1)/fs;
+               surface(tms(:,1:StepSize:end),frq,abs(cfs(:,1:StepSize:end)));
+        
+                set(gca, 'Visible', 'off')
+                colorbar('off');
+                InSet = get(gca, 'TightInset');
+                set(gca, 'Position', [InSet(1:2), 1-InSet(1)-InSet(3), 1-InSet(2)-InSet(4)]);
+                axis tight
+                shading flat
+                set(gca,'yscale','log')
+              saveas(gcf,strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'))
+              I = imread(strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
+               J = imresize(I, 0.5);
+              imwrite(J,strcat(PathTo,'\cwt_',int2str(imindex),'.jpg'));
+                close all
+                imindex= imindex+1;
+        %
+        oneSecond = zeros(44100,1);
         i = 1;
-        clear I 
-        clear J
-        totoverrun
-        toc
-  
+                clear I
+                clear J
+                totaloverrun
+
+
     end
+
 end
 disp('Recording complete.')
 release(deviceReader)
 release(fileWriter)
-
