a89dc9b3cfb31d79f03f221cfab390a3f52de706
jennyhoglund <56074011+jennyhoglund@users.noreply.github.com>
2022-05-16 11:29:25 +0200

    Update tableAudio.m

--- tableAudio.m
+++ tableAudio.m
@@ -1,25 +1,123 @@
+
 clc
 clearvars
-t= readtable('C:\Users\hebam\Desktop\Examensarbete\Code_MATLAB/ES2015a.A.xls');
-[y,fs]= audioread('C:\Users\hebam\Desktop\Examensarbete\AMI-corpus/ES2015a.Mix-Lapel.wav');
-fs
+
+maintbl = readtable("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\excelfiler\es2011a-A");
+tbl0 = readtable("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\excelfiler\es2011a-B");
+tbl2 = readtable("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\excelfiler\es2011a-C");
+tbl3 = readtable("C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\excelfiler\es2011a-D");
+
+audioPath ="C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\ljud"
+audiomixfile = "ES2011a";
+[y, fs] = audioread(strcat(audioPath, "\",audiomixfile, ".wav"));
+
+temp1 = [300 350 ; 350 370];
+temp2 = [310 331 ; 340 380];
+%%
+
+for i = 1:height(maintbl)
+   maintbl = removeOverlapLoop(maintbl, tbl0, i);
+   maintbl = removeOverlapLoop(maintbl, tbl2, i);
+   maintbl = removeOverlapLoop(maintbl, tbl3, i);
+end
+filtertable(maintbl,audioPath,audiomixfile);
+
+%%
+
+
+function newtbl = removeOverlapLoop(maintbl, tbl1, i)
+start = maintbl.start(i);
+en = maintbl.xEnd(i);
+en2 = 0;
+j = 1;
+while en2 <= en && j < height(tbl1)
+
+    start2 = tbl1.start(j);
+    en2 = tbl1.xEnd(j);
+    %j = j+1
+    if start2 >start && en2 < en
+        disp('segment1 -segment2- segment1')
+        if (start2 - start) > (en - en2)
+            en = start2;
+            maintbl.xEnd(i) = en;
+            %  temp1(i,2) = en
+        else
+
+            start = en2;
+            maintbl.start(i) = start;
+            %  temp1(i,1) = start
+
+        end
+
+    elseif start2 == start
+        disp('start at the same time')
+        maintbl.xEnd(i) = maintbl.start(i);
+        %  temp1(i,2) = temp1(i,1)
+
+    elseif en2 > start && en2 < en
+        disp('start1 < en2 < en')
+        start = en2;
+        maintbl.start(i) = start;
+
+    elseif start2 < start && en2>en
+        disp('segment2 -segment1- segment2')
+        en = start;
+        maintbl.xEnd(i) = en;
+
+    elseif start2 > start  && start2 < en
+        disp(['start1 < start2 < en1'])
+        en = start2;
+        maintbl.xEnd(i) = en;
+
+    end
+    j = j+1;
+end
+newtbl = maintbl;
+end
+function filtertable(maintbl, audioPath, audiomixfile)
+[audio, fs] = audioread(strcat(audioPath, "\",audiomixfile, ".wav"));
+%
+% tablePath = "C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\excelfiler"
+% audioPath = "C:\Users\jenny\OneDrive\Documents\Skola\Examensarbete\Material\nyaAmiCorpus\lapelmixes\ljud"
+% audiomixfile = "ES2011a"
+% tablename = "es2011a-A"
+
+
+% t = readtable(strcat(tablePath,"\", tablename, ".xls"))
+
+
 lastend=1;
-onesec= zeros(floor(fs*(sum(t.xEnd(1:end))-sum(t.start(1:end)))),1);
+onesec= zeros(floor(fs*(sum(maintbl.xEnd(1:end))-sum(maintbl.start(1:end)))),1);
 totaltime = 0;
-for i=1:height(t)
-    
- start=floor(fs*t.start(i))
 
- endd=floor( fs*t.xEnd(i));
- x = y(start:endd-1);
- a = endd-start;
 
- totaltime = totaltime+a/fs
- length(x)
-% length(onesec(lastend:lastend+a-1))
- onesec(lastend:lastend+a-1)=x;
- lastend=lastend+ a+1;
+
+
+% Läs in alla tabeller, för varje rad i tabellen som ljudfil ska göras för,
+% kolla jämförs dess starttid och sluttid med alla andra. Leta igenom de
+% andra tabellerna tills man kommit till en rad som startar efter att den
+% första slutar. Om en annan tabell har en starttid som är efter den
+% första, men som startar innan den första slutar, ta bort del av
+% segmentet. Om en tabell har en starttid som är innan den första men som
+% slutar efter den första slutar. I princip, om antingen sluttiden eller
+% starttiden för någon rad är mellan start till slut för den aktuella
+% raden, ta bort. ta också bort om starttiden är exakt samtdigigt. om en
+% bit av signalen måste tas bort, skriv in nya start och sluttiden. Om
+% startiderna är samma, ta bort hela segmentet. I den inre loopen, sök
+% igenom en tabell tills dess sluttid är efter segmentet.
+
+
+
+for i=1:height(maintbl)
+    start=floor(fs*maintbl.start(i))
+    endd=floor( fs*maintbl.xEnd(i));
+    x = audio(start:endd-1);
+    a = endd-start;
+    totaltime = totaltime+a/fs
+    onesec(lastend:lastend+a-1)=x;
+    lastend=lastend+ a+1;
 
 end
 
- audiowrite('ES2015a.wav',onesec,fs)
+audiowrite(strcat(audioPath, "\",audiomixfile, "_filtered.wav"),onesec,fs);
+end
